93e65b8601627d6146e46887db43dee2b988c68a
Zengyu Lu <dev@zengyu.io>
2025-02-15 12:37:12 +0000

    add script for 3b-instruct

--- examples/data_preprocess/countdown.py
+++ examples/data_preprocess/countdown.py
@@ -68,7 +68,7 @@ Assistant: Let me solve this step by step.
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--local_dir', default='~/data/countdown')
+    parser.add_argument('--local_dir', default='./data/countdown')
     parser.add_argument('--hdfs_dir', default=None)
     parser.add_argument('--num_samples', type=int, default=100000)
     parser.add_argument('--num_operands', type=int, default=6)
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     parser.add_argument('--min_number', type=int, default=1)
     parser.add_argument('--max_number', type=int, default=100)
     parser.add_argument('--train_size', type=int, default=327680)
-    parser.add_argument('--test_size', type=int, default=1024)
+    parser.add_argument('--test_size', type=int, default=2048)
     parser.add_argument('--template_type', type=str, default='base')
 
     args = parser.parse_args()
--- load_and_test.py
+++ load_and_test.py
@@ -1,17 +1,33 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
+torch.cuda.set_stream(torch.cuda.Stream())
+
 # 指定本地模型路径（替换为你的实际路径）
 model_path = "/home/zengyu/Workspace/2_models/countdown_trained"
 #model_path = "/home/zengyu/Workspace/2_models/Qwen2/Qwen2.5-3B"
+##model_path = "/home/zengyu/Workspace/2_models/deepseek/DeepSeek-R1-Distill-Qwen-32B"
+
+
+# 修改模型加载方式
+from transformers import BitsAndBytesConfig
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
 
 # 加载模型和分词器
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(
     model_path,
+    #quantization_config=bnb_config,
     trust_remote_code=True,
     device_map="auto",  # 自动分配设备（GPU/CPU）
-    torch_dtype='auto' #torch.float16  # 自动处理数据类型
+    torch_dtype=torch.float16  # 自动处理数据类型
 )
 
 # 验证设备分配
@@ -31,7 +47,7 @@ def generate_text(prompt, max_length=1024):
         "attention_mask": inputs.attention_mask,
         "max_new_tokens": max_length,
         "do_sample": True,  # 启用随机采样
-        "temperature": 0.8,  # 控制随机性（0.1-1.0）
+        "temperature": 1.10,  # 控制随机性（0.1-1.0）
         "top_p": 0.9,        # 核采样参数
         "pad_token_id": tokenizer.eos_token_id  # 设置结束符为填充符
     }
@@ -46,6 +62,6 @@ def generate_text(prompt, max_length=1024):
 
 # 使用示例
 if __name__ == "__main__":
-    prompt = "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer.\nUser: Using the numbers [73, 76, 6, 70], create an equation that equals 38. You can use basic arithmetic operations (+, -, *, /) and each number can only be used once. Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags, for example <answer> (1 + 2) / 3 </answer>.\nAssistant: Let me solve this step by step.\n<think>"
+    prompt = "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer.\nUser: Using the numbers [87, 58, 13, 59], create an equation that equals 74. You can use basic arithmetic operations (+, -, *, /) and each number can only be used once. Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags, for example <answer> (1 + 2) / 3 </answer>.\nAssistant: Let me solve this step by step.\n<think>"
     print("输入提示:", prompt)
     print("生成结果:", generate_text(prompt))
